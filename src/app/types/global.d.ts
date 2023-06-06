605d4d198f1216db341ec9f33d3cf91d0b4c47f4
Ildar <ildarvaliev167@gmail.com>
2023-06-06 13:14:24 +0300

    добавил eslint, поправил ошибки в ts, tsx

--- src/app/types/global.d.ts
+++ src/app/types/global.d.ts
@@ -6,15 +6,16 @@ declare module '*.scss' {
   export = classNames;
 }
 
-declare module "*.png";
-declare module "*.jpg";
-declare module "*.jpeg";
-declare module "*.gif";
-declare module "*.woff";
-declare module "*.woff2";
+declare module '*.png';
+declare module '*.jpg';
+declare module '*.jpeg';
+declare module '*.gif';
+declare module '*.woff';
+declare module '*.woff2';
 
-declare module "*.svg" {
-  import React from "react";
+declare module '*.svg' {
+  import React from 'react';
+
   const SVG: React.VFC<React.SVGProps<SVGSVGElement>>;
   export default SVG;
 }
