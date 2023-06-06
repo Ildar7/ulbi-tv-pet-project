1908bcc351cbd7a5486ed748758a1e920db04e8e
Ildar <ildarvaliev167@gmail.com>
2023-06-06 12:13:12 +0300

    добавил интернационализацию i18n, плагин webpack HMR

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -2,8 +2,9 @@ import HTMLWebpackPlugin from "html-webpack-plugin";
 import webpack from "webpack";
 import {BuildOptions} from "./types/config";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import ReactRefreshWebpackPlugin from "@pmmmwh/react-refresh-webpack-plugin";
 
-export function buildPlugins({paths}: BuildOptions): webpack.WebpackPluginInstance[] {
+export function buildPlugins({paths, isDev}: BuildOptions): webpack.WebpackPluginInstance[] {
   return [
     new HTMLWebpackPlugin({
       template: paths.html
@@ -13,5 +14,10 @@ export function buildPlugins({paths}: BuildOptions): webpack.WebpackPluginInstan
       filename: 'css/[name].[contenthash:8].css',
       chunkFilename: 'css/[name].[contenthash:8].css',
     }),
+    new webpack.DefinePlugin({
+      __IS_DEV__: JSON.stringify(isDev)
+    }),
+    (isDev ? new webpack.HotModuleReplacementPlugin() : undefined),
+    (isDev ? new ReactRefreshWebpackPlugin() : undefined)
   ]
 }
